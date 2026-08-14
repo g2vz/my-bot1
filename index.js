3716a864b5a6b2498b550a319c018d8e2dd21f33
g2vz <renamashiro1@gmail.com>
2026-08-15 01:39:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const levelSystem = require("./levelling-system.js");
 const gamesSystem = require("./games-system.js");
+
+const {
+  Client,
+  GatewayIntentBits,
+  Events,
+} = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+});
