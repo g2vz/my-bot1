6d6da4098c791430a46045351443f683ca3560d1
g2vz <renamashiro1@gmail.com>
2026-08-29 07:11:08 +0300

    Update Nexona.js

--- Nexona.js
+++ Nexona.js
@@ -12,6 +12,7 @@ const automod2 = require("./automod2");
 const softban = require("./softban");
 const chm = require ("./chm");
 const warning = require ("./warning");
+const automodfix = require ("./automodfix");
 
 const client = new Client({
     intents: [
